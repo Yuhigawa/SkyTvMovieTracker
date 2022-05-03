c66d5c814c2fa087f4a487b2a265fe125e0a53b6
yuhigawa <pablostreiski@outlook.com>
2022-05-03 12:59:37 -0300

    Recoding on a life the old date

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const server = require('express')();
+const { throws } = require('assert');
 const cors = require('cors');
 const fs = require('fs');
 
 server.use(cors());
 
 const scrapper = require('./src/sky-tv-scrapper');
-let old_date = new Date()
-let old_date_day = old_date.toString().split(" ")[2];
 
 server.get('/', async (req, res) => {   
+    let old_date = new Date();
     const new_date = new Date();
     const new_date_day = new_date.toString().split(" ")[2];
     
+    try {
+        const date_record = fs.readFileSync('./src/data/date_record.txt', {encoding:'utf8', flag:'r'});
+        
+        if( !date_record.length ) {
+            console.log('Writing file');
+            fs.writeFileSync('./src/data/date_record.txt', old_date.toString());
+        }
+        else {
+            console.log('Reading date_record');
+            old_date = new Date(date_record);
+        }
+    } catch (error) {
+        console.error(`Error at reading file with old date ${error}`);
+    }
+    
     console.log('Old Date: ', old_date)
     console.log('New Date: ', new_date)
+    
+    old_date_day = old_date.toString().split(" ")[2];
     
     if( new_date_day !== old_date_day ) {
         console.log('\n\tDifferent Day\n');
@@ -28,16 +45,18 @@ server.get('/', async (req, res) => {
         try {
             const data = fs.readFileSync('./src/data/skyTvScrapped.json', {encoding:'utf8', flag:'r'});
 
+            console.dir(data.length)
+
             if( data.length <= 2 ){
                 console.log('\n\tSame Day Writing File\n');
 
                 const data = await scrapper(true);
                 res.send( data );
             }
-
-            console.log('\n\tSame Day Reading File\n');
-
-            res.send( data );
+            else{
+                console.log('\n\tSame Day Reading File\n');
+                res.send( data );
+            }
         } catch (error) {
             res.status(500).send({message: 'Couldnt get data'});
         }
